cb039f2eff67ab9b7929fd6ba67c437ad3812d5e
Ben <mineeraft@gmail.com>
2021-04-10 16:21:35 +1200

    Load files from static folder

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')));
+
 app.get('/', (req, res) => {
-	res.send('Hello express here :3');
+	res.sendFile(path.join(__dirname, './static/index.html'));
 });
 
 app.listen(port, () => {
