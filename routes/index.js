e2798c2737f4b07d892ee96375850a4a1253cb6f
Ben <mineeraft@gmail.com>
2021-04-11 13:34:04 +1200

    Add session visit counter

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ const router = express.Router();
 
 module.exports = (params) => {
 	router.get('/', (req, res) => {
+		if (!req.session.visitcount) {
+			req.session.visitcount = 0;
+		}
+
+		req.session.visitcount += 1;
+		console.log(`Number of visits: ${req.session.visitcount}`);
+
 		res.render('pages/index', { pageTitle: 'Welcome' });
 	});
 
