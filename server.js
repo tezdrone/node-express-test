82075fed290b2f3f807a161fb96eea0da71274c5
Ben <mineeraft@gmail.com>
2021-04-11 13:15:47 +1200

    Move route to router

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express();
 
 const port = 3000;
@@ -10,9 +12,7 @@ app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (req, res) => {
-	res.render('pages/index', { pageTitle: 'Welcome' });
-});
+app.use('/', routes());
 
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);
