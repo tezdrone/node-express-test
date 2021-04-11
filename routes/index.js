2f6eff744523e01070498caad2df0add72b3db59
Ben <mineeraft@gmail.com>
2021-04-11 13:45:52 +1200

    move header and footer to partial

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,7 @@ const router = express.Router();
 
 module.exports = (params) => {
 	router.get('/', (req, res) => {
-		if (!req.session.visitcount) {
-			req.session.visitcount = 0;
-		}
-
-		req.session.visitcount += 1;
-		console.log(`Number of visits: ${req.session.visitcount}`);
-
-		res.render('pages/index', { pageTitle: 'Welcome' });
+		res.render('layout', { pageTitle: 'Welcome', template: 'index' });
 	});
 
 	router.use('/speakers', speakersRoute(params));
