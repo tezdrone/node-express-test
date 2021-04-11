d65e50124aeef91db0f68656970b41759ddbb1cc
Ben <mineeraft@gmail.com>
2021-04-11 13:05:04 +1200

    Add EJS

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 
 const port = 3000;
 
+app.set('view enjine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static(path.join(__dirname, './static')));
 
 app.get('/', (req, res) => {
